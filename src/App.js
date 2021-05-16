25033cd937e5a122d892e5603520defbcc58669d
Ezure-HARUNA <haruna.e@80and.co>
2021-05-16 16:10:57 +0900

    useEffectでコールバック実装

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-//複数の状態ウィオブジェクトに統合してリファクタリングする方法
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = props => {
-  const [ state, setState ] = useState(props)
+  const [state, setState] = useState(props)
   const { name, price } = state
+
+  useEffect(() => {
+    console.log('useEffect is invoked')
+  })
+
+  useEffect(() => {
+    console.log('This callback is for name only');
+  }, [])
+
   return (
     <>
-      <p>現在の{state.name}は、{state.price}円です。</p>
-      <button onClick={() => setState({...state, price: state.price +1})}>+1</button>
-      <button onClick={() => setState({...state, price: state.price -1})}>-1</button>
+      <p>現在の{name}は、{price}円です.</p>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
       <button onClick={() => setState(props)}>Reset</button>
-      <input value={state.name} onChange={(e) => setState({...state, name: e.target.value})} />
+      <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
     </>
   )
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App
